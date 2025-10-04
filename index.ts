dc38d7e5c88d4ac286385e2377c266d229faefa6
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 11:09:11 -0700

    Refactored package to implement automatic installation & configuration in TokenRing apps

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-import type { TokenRingPackage } from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
 
 import * as chatCommands from "./chatCommands.ts";
-import packageJSON from "./package.json" with { type: "json" };
+import packageJSON from "./package.json" with {type: "json"};
+import ShortTermMemoryService from "./ShortTermMemoryService.js";
 import * as tools from "./tools.ts";
 
 export const packageInfo: TokenRingPackage = {
 	name: packageJSON.name,
 	version: packageJSON.version,
 	description: packageJSON.description,
-	chatCommands,
-	tools,
+  install(agentTeam: AgentTeam) {
+    agentTeam.addTools(packageInfo, tools)
+    agentTeam.addChatCommands(chatCommands);
+    agentTeam.addServices(new ShortTermMemoryService());
+  },
 };
 
 export { default as ShortTermMemoryService } from "./ShortTermMemoryService.ts";
